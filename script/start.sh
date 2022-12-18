1b15ae72e6d636896539d5371f53049e698c73c3
yinchuangsum <yinchuang96@gmail.com>
2022-12-18 21:45:02 +0800

    scripts

---
+++ script/start.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+nohup java -jar /usr/local/mayair-aqi/web.jar > log.log 2>&1 &
---
+++ script/stop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pid=`ps -ef | grep java | grep web.jar | awk '{printf $2}'`
+
+if [ $pid ];
+then
+  kill -9 $pid
+else
+  echo "app not started"
+fi
